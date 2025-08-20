bc6aed83c6390d7f623caad4605498ed4dc4831a
dankeschoen13 <marcobernacer@gmail.com>
2025-08-20 22:43:55 +0800

    change starting Tkinter GUI layout

--- main.py
+++ main.py
@@ -12,18 +12,19 @@ class TypingTest(tk.Tk):
         super().__init__()
 
         self.title("Typing Test")
-        self.geometry("800x450")
+        self.minsize(800, 500)
         self.config(padx=50, pady=50)
 
         self.UI = dict()
-        self.UI['toolbar'] = self.render_toolbar()
+        self.UI['toolbar'] = self.render_top_toolbar()
         self.UI['main'] = self.render_body()
+        self.UI['more_options'] = self.render_bottom_toolbar()
 
         self.diff_select()
 
-    def render_toolbar(self):
+    def render_top_toolbar(self):
         toolbar = tk.Frame(self, highlightbackground='white', highlightthickness=1)
-        toolbar.pack(side="top", fill="x")
+        toolbar.pack(side='top', fill='x')
 
         radiovar = tk.StringVar()
         radiovar.set('easy')
@@ -35,7 +36,7 @@ class TypingTest(tk.Tk):
             value='easy',
             command=self.diff_select
         )
-        easy.pack(side="left", padx=5, pady=10)
+        easy.pack(side='left', padx=5, pady=10)
 
         medium = tk.Radiobutton(
             toolbar,
@@ -44,7 +45,7 @@ class TypingTest(tk.Tk):
             value='medium',
             command = self.diff_select
         )
-        medium.pack(side="left", padx=5, pady=10)
+        medium.pack(side='left', padx=5, pady=10)
 
         hard = tk.Radiobutton(
             toolbar,
@@ -53,7 +54,7 @@ class TypingTest(tk.Tk):
             value='hard',
             command=self.diff_select
         )
-        hard.pack(side="left", padx=5, pady=10)
+        hard.pack(side='left', padx=5, pady=10)
 
         speed = tk.Label(
             toolbar,
@@ -61,46 +62,68 @@ class TypingTest(tk.Tk):
             bg='white',
             fg='black'
         )
-        speed.pack(side="right", padx=5, pady=10)
+        speed.pack(side='right', padx=5, pady=10)
 
         speed_label = tk.Label(
             toolbar,
             text="Words per minute:"
         )
-        speed_label.pack(side="right", padx=5, pady=10)
+        speed_label.pack(side='right', padx=5, pady=10)
 
         return {'radiovar': radiovar, 'speed': speed}
 
     def render_body(self):
+        paned = tk.PanedWindow(
+            self,
+            orient="vertical"
+        )
+        paned.pack(fill='both', expand=True)
+
         ttext = tk.Text(
-            master=self,
+            master=paned,
             wrap='word',
             width=100,
-            height=10
+            height=10,
+            highlightthickness=0,
         )
         ttext.bind("<Key>", input_blocker)
-        ttext.pack()
+        ttext.pack(side="top", fill="both")
 
         user_input = tk.Text(
-            master=self,
+            master=paned,
             wrap='word',
             width=100,
-            height=10
+            height=10,
+            highlightthickness=0
         )
-        user_input.pack()
+        user_input.pack(side="top", fill="both")
 
+        paned.add(ttext)
+        paned.add(user_input)
+
+        return {'ttext': ttext, 'user_input': user_input}
+
+    def render_bottom_toolbar(self):
         options = tk.Frame(
             self,
             highlightthickness=1,
             highlightbackground='white'
         )
+        options.pack(side="bottom", fill="x")
+
         start_button = tk.Button(
             options,
-            text="Start",
+            text="Start"
         )
-        start_button.place(relx=0.5, rely=0.5)
+        start_button.pack(side='left', padx=5, pady=10)
 
-        return {'ttext': ttext, 'user_input': user_input}
+        timer = tk.Label(
+            options,
+            text="Timer:"
+        )
+        timer.pack(side='right', padx=5, pady=10)
+
+        return timer
 
     def diff_select(self):
         try:
@@ -113,8 +136,6 @@ class TypingTest(tk.Tk):
     def set_ttext_content(self, content):
         self.UI['main']['ttext'].delete("1.0", "end")
         self.UI['main']['ttext'].insert('1.0', content)
-
-
 
 
 if __name__ == "__main__":
